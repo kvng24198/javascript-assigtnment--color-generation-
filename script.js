df5c2d81fac9a7a6d5530b1544c9f4abee0afe82
kvng24198 <kvngsamuel24198@gmail.com>
2024-12-09 09:02:46 -0800

    done

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 "use strict";
 
-const container = document.querySelector(".container");
 const contentBox = document.querySelector(".content-box");
 const colorGen = document.querySelector(".color-gen");
 const colorBtn = document.querySelector(".color-btn");
-
+const body = document.querySelector("body");
 const color = [
   "#FF69B4",
   "#33CC33",
@@ -17,24 +16,14 @@ const color = [
   "#666666",
   "#CCCCCC",
 ];
-for (let x = 0; x < color.length; x++) {
-  //   console.log(color[x]);
-  const randomIndex = Math.floor(math.radom() * color.length);
-  const randomColor = color[randomIndex];
-  //   return randomColor;
-}
 
-// function generateRandomColor() {
-//   const randomcolor = math.floor(math, random() * color.length);
-//   // Get the color at the random index
-//   const randomColor = color[randomIndex];
-
-//   // Display the random color
-//   colorDisplay.style.backgroundColor = randomColor;
-//   colorDisplay.innerText = randomColor;
-//   return randomcolor;
-// }
-// const randomColor = color[x];
 colorBtn.addEventListener("click", function () {
-  container.style.backgroundColor = "red";
+  //generate a random number
+  let randomNumber = Math.trunc(Math.random() * 10);
+  //change the backgroundcolor of the body
+  contentBox.style.backgroundColor = color[randomNumber];
+  body.style.backgroundColor = color[randomNumber];
+  colorBtn.style.backgroundColor = color[randomNumber];
+  // display the color value in the div box
+  colorGen.textContent = color[randomNumber];
 });
